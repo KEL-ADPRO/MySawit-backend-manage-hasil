52f1396be988d1a28f8b22978163ccec8020bf0c
NaviCo-de <aridarrell30@gmail.com>
2026-02-17 23:20:39 +0700

    Early Setup:

---
+++ src/main/java/com/mysawit/mysawit_panen/MySawitPanenApplication.java
@@ -0,0 +1,13 @@
+package com.mysawit.mysawit_panen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySawitPanenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySawitPanenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mysawit/mysawit_panen/MySawitPanenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mysawit.mysawit_panen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySawitPanenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
