2a0759e15a7ea7949543ba7fa24949f3e1030647
msatrioh <90437153+msatrioh@users.noreply.github.com>
2026-02-19 22:57:59 +0700

    added sonar in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
 	java
 	id("org.springframework.boot") version "3.5.10"
 	id("io.spring.dependency-management") version "1.1.7"
+	id("org.sonarqube") version "4.4.1.3373"
 }
 
 group = "com.mysawit"
@@ -32,6 +33,18 @@ dependencies {
 	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+sonar {
+	properties {
+		property("sonar.projectKey", "KEL-ADPRO_MySawit-backend-manage-hasil")
+
+		property("sonar.organization", "kel-adpro")
+
+		property("sonar.host.url", "https://sonarcloud.io")
+
+		property("sonar.coverage.jacoco.xmlReportPaths", "build/reports/jacoco/test/jacocoTestReport.xml")
+	}
 }
 
 tasks.withType<Test> {
