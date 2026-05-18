81b3a5c856d13532b233551fa3dce20359e144b5
Benhanan14406 <a.b.abinaya.fuun@gmail.com>
2026-05-18 09:11:25 +0700

    Implemented PayrollEventPublisher

--- src/main/java/com/mysawit/mysawit_panen/service/PayrollEeventPublisherImpl.java
+++ src/main/java/com/mysawit/mysawit_panen/service/PayrollEeventPublisherImpl.java
@@ -1,14 +1,20 @@
 package com.mysawit.mysawit_panen.service;
 
+import com.mysawit.mysawit_panen.client.PayrollClient;
 import com.mysawit.mysawit_panen.model.HarvestReport;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class PayrollEeventPublisherImpl implements PayrollEventPublisher{
+    private final PayrollClient pembayaranClient;
+
     @Override
     public void publishHarvestApprovedEvent(HarvestReport report) {
-
+        log.info("Publishing harvest approved event for buruh={}, weight={}kg", report.getBuruhId(), report.getWeight());
+        pembayaranClient.createPayroll(report.getBuruhId(), report.getWeight());
     }
 }
