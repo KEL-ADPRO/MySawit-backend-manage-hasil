a3fa0c661b23004f40c8f68a6b366787de99244e
Benhanan14406 <a.b.abinaya.fuun@gmail.com>
2026-05-18 14:10:25 +0700

    Activates the test's profile

--- src/test/java/com/mysawit/mysawit_panen/MySawitPanenApplicationTests.java
+++ src/test/java/com/mysawit/mysawit_panen/MySawitPanenApplicationTests.java
@@ -2,8 +2,10 @@ package com.mysawit.mysawit_panen;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class MySawitPanenApplicationTests {
 
 	@Test
